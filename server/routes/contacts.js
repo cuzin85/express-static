58f5fc1fd23b7a4f1ff3018ca1520c9f5ad3b049
DESKTOP-18FQN97\osapp <osappp@gmail.com>
2017-09-15 16:27:47 +0300

    add ejs include

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import {nav} from '../data';
 
 export const router = express.Router();
 
 router.route('/')
   .get((req, res) => {
-    res.render('contacts');
+    res.render('contacts', {nav});
   });
 
 router.route('/some')
